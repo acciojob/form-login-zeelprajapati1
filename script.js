263e2b4fa181b498f804cb3869e6f6d9ca50fdb1
zeelprajapati1 <zeelprajapati9876@gmail.com>
2025-03-26 03:35:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
-function getFormvalue(event) {
-    event.preventDefault(); // Prevent form from refreshing the page
+function getFormvalue() {
+    //Write your code here
 
-    let firstName = document.querySelector("input[name='fname']").value.trim();
-    let lastName = document.querySelector("input[name='lname']").value.trim();
-
-    if (firstName === "" || lastName === "") {
-        alert("Please enter both first and last name.");
-    } else {
-        alert(`${firstName} ${lastName}`);
-    }
 }
-
-// Attach the function to the form submission
-document.addEventListener("DOMContentLoaded", function() {
-    document.querySelector("form").addEventListener("submit", getFormvalue);
-});
